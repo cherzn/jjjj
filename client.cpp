ecf9d8bb3a33e02f62267b8cf201076a78c85f37
cherzn <polinacheredina@gmail.com>
2025-04-13 23:40:37 +0300

    изменения

--- client.cpp
+++ client.cpp
@@ -9,6 +9,73 @@
 const int PORT = 8080;
 std::atomic<bool> running(true);
 std::string name;
+std::string encryption_key; // Кодировочное слово
+
+size_t utf8_length(const std::string &str) {
+    size_t length = 0;
+    for (size_t i = 0; i < str.length(); ++i) {
+        if ((str[i] & 0x80) == 0) {
+            length++;
+        } else if ((str[i] & 0xE0) == 0xC0) {
+            length++;
+            i++;
+        } else if ((str[i] & 0xF0) == 0xE0) {
+            length++;
+            i += 2;
+        } else if ((str[i] & 0xF8) == 0xF0) {
+            length++;
+            i += 3;
+        }
+    }
+    return length;
+}
+
+std::string encrypt_message(const std::string &message, const std::string &kode) {
+    size_t cols = utf8_length(kode);
+    size_t message_length = utf8_length(message);
+    size_t rows = (message_length / cols) + (message_length % cols == 0 ? 0 : 1);
+
+    char matrix[100][401] = {};
+    size_t index = 0;
+
+    // Заполнение матрицы
+    for (size_t i = 0; i < rows; ++i) {
+        for (size_t j = 0; j < cols; ++j) {
+            if (index < message.length()) {
+                size_t char_length = 1;
+                if ((message[index] & 0x80) == 0) {
+                    char_length = 1;
+                } else if ((message[index] & 0xE0) == 0xC0) {
+                    char_length = 2;
+                } else if ((message[index] & 0xF0) == 0xE0) {
+                    char_length = 3;
+                } else if ((message[index] & 0xF8) == 0xF0) {
+                    char_length = 4;
+                }
+
+                for (size_t k = 0; k < char_length; ++k) {
+                    matrix[i][j * 4 + k] = (index + k < message.length()) ? message[index + k] : ' ';
+                }
+                index += char_length;
+            }
+        }
+    }
+
+    std::string encrypted_message;
+
+    // Формирование зашифрованного сообщения
+    for (size_t j = 0; j < cols; ++j) {
+        for (size_t i = 0; i < rows; ++i) {
+            for (size_t k = 0; k < 4; ++k) {
+                if (matrix[i][j * 4 + k] != 0) {
+                    encrypted_message += matrix[i][j * 4 + k];
+                }
+            }
+        }
+    }
+
+    return encrypted_message;
+}
 
 void receive_messages(int client_socket) {
     char buffer[1024];
@@ -34,8 +101,11 @@ void send_messages(int client_socket) {
             break;
         }
 
-        // Отправляем на сервер
-        send(client_socket, message, strlen(message), 0);
+        // Шифрование сообщения
+        std::string encrypted_message = encrypt_message(message, encryption_key);
+
+        // Отправляем зашифрованное сообщение на сервер
+        send(client_socket, encrypted_message.c_str(), encrypted_message.size(), 0);
     }
 }
 
@@ -65,6 +135,9 @@ int main() {
 
     std::getline(std::cin, name); // Ввод имени клиента
     send(client_socket, name.c_str(), name.size(), 0);
+
+    std::cout << "Введите код шифрования (русские буквы): ";
+    std::getline(std::cin, encryption_key); // Ввод кодировочного слова
 
     // Запускаем поток для получения сообщений
     std::thread(receive_messages, client_socket).detach();
